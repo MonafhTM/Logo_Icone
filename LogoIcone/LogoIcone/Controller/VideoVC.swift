0d567593e52b06d54f65d73d8559d826602140ef
Monafh <name@gmail.com>
2022-01-15 21:22:14 +0300

    dark/lighs &   Localiza

--- LogoIcone/LogoIcone/Controller/VideoVC.swift
+++ LogoIcone/LogoIcone/Controller/VideoVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoVC: UIViewController {
 
@@ -15,9 +17,15 @@ class VideoVC: UIViewController {
 
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidApper(_ animated: Bool) {
+        super.viewDidApper(animated)
 
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "", ofType: "m4v")!))
+        
+        let vc = AVPlayerViewController()
+        vc.player = player
+        present(vc, animated: true)
+        
         
     }
     
